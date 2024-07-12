1e0b20d88edf76100396b6790b8c245084f1658f
wallace <wallace_wolv@hotmail.com>
2024-07-11 23:47:06 -0300

    refactor: Componentizando InviteGuestsStep

--- src/pages/create-trip/index.tsx
+++ src/pages/create-trip/index.tsx
@@ -1,10 +1,10 @@
-import { ArrowRight, UserRoundPlus } from 'lucide-react';
 import { FormEvent, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import { ConfirmTripModal } from './confirm-trip-modal';
-import { DestinationAndDateStep } from './steps/destination-and-date-step';
 import { InviteGuestsModal } from './invite-guests-modal';
+import { DestinationAndDateStep } from './steps/destination-and-date-step';
+import { InviteGuestsStep } from './steps/invite-guests-step';
 
 export function CreateTripPage() {
     const navigate = useNavigate();
@@ -91,25 +91,11 @@ export function CreateTripPage() {
                     />
 
                     {isGuestsInputOpen && (
-                        <div className="h-16 bg-zinc-900 px-4 rounded-xl flex items-center shadow-shape gap-3">
-                            <button type='button' onClick={openGuestsModal} className='flex items-center gap-2 flex-1 text-left'>
-                                <UserRoundPlus className='size-5 text-zinc-400' />
-                                {emailsToInvite.length > 0 ? (
-                                    <span className='text-zinc-100 text-lg flex-1'>
-                                        {emailsToInvite.length} pessoa(s) convidada(s)
-                                    </span>
-                                ) : (
-                                    <span className='text-zinc-400 text-lg flex-1'>Quem estará na viagem?</span>
-                                )}
-                            </button>
-
-                            <div className='w-px h-6 bg-zinc-800' />
-
-                            <button onClick={openConfirmTripModal} className='bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400'>
-                                Confirmar viagem
-                                <ArrowRight className='size-5' />
-                            </button>
-                        </div>
+                        <InviteGuestsStep
+                            emailsToInvite={emailsToInvite}
+                            openConfirmTripModal={openConfirmTripModal}
+                            openGuestsModal={openGuestsModal}
+                        />
                     )}
                 </div>
 
